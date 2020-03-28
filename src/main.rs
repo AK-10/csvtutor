4b234300cad5988b9eb63e4f6e1574fac2bb67d3
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-03-28 20:14:07 +0900

    sec1 setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+// csv crateをプログラムから利用可能にする
+extern crate csv;
+
+use std::error::Error;
+use std::io;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
 }
+
+// trait objects without an explicit dyn are deprecated
+fn run() -> Result<(), Box<Error>> {
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+
+    for result in rdr.records() {
+        // ?: matchのシンタックスシュガー Result を返す関数内でのみ使える
+        let record = result?;
+        println!("{:?}", record);
+        // match result {
+        //     Err(err) => return Err(From::from(err)),
+        //     Ok(record) => {
+        //         println!("{:?}", record);
+        //     }
+        // }
+    }
+
+    Ok(())
+}
+
