55ef64d3167f669a4ddde584ad83f1886348ef46
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-04-12 02:53:30 +0900

    tutorial-read-headers-02

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,31 @@ fn main() {
 // Box<T>はヒープのデータを指す
 // https://doc.rust-jp.rs/book/second-edition/ch15-01-box.html
 fn run() -> Result<(), Box<Error>> {
-    let mut rdr = csv::ReaderBuilder::new()
-        .has_headers(false)
-        .from_reader(io::stdin());
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+
+    // lifetimeを明確にするためのスコープ
+    // rdr.haedersはborrowを返す
+    // CSVリーダーのヘッダからのborrowと、CSVリーダーがレコード上をイテレートしようとするとき必要なborrowを、同時に行うことはできない
+    // とあったが，ここはなくてもコンパイルされた
+    // https://doc.rust-jp.rs/book/second-edition/ch04-02-references-and-borrowing.html
+    // https://doc.rust-jp.rs/the-rust-programming-language-ja/1.6/book/references-and-borrowing.html
+    {
+        let headers = rdr.headers()?;
+        println!("{:?}", headers);
+    }
+
 
     for result in rdr.records() {
         let record = result?;
         println!("{:?}", record)
     }
+
+    // 原文
+    // We can ask for the headers at any time. There's no need to nest this
+    // call in its own scope because we never try to borrow the reader again.
+    // これ以降reader(rdr)は呼ばれないのでネストする必要はない
+    let headers = rdr.headers()?;
+    println!("{:?}", headers);
 
     Ok(())
 }
