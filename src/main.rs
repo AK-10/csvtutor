faa319dd31fcf57d15692acbf2f34f4aa5ae41b4
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-04-12 02:27:56 +0900

    tutorial-read-01

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,37 @@
+// csv crateをプログラムから利用可能にする
 extern crate csv;
 
-use csvtutor::sess1_setup;
+use std::env;
+use std::error::Error;
+use std::ffi::OsString;
+use std::fs::File;
+use std::process;
 
 fn main() {
-    sess1_setup::main();
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
+
+// Box<T>はヒープのデータを指す
+// https://doc.rust-jp.rs/book/second-edition/ch15-01-box.html
+fn run() -> Result<(), Box<Error>> {
+    let file_path = get_first_arg()?;
+    let file = File::open(file_path)?;
+    let mut rdr = csv::Reader::from_reader(file);
+
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record)
+    }
+
+    Ok(())
+}
+
+fn get_first_arg() -> Result<OsString, Box<Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path)
+    }
 }
