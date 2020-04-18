f7ffef257368dc623217c92f050987fd3ce1b234
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-04-18 21:12:14 +0900

    tutorial-write-02

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use csvtutor;
 
 fn main() {
-    csvtutor::write::tutorial_write_01::main();
+    // csvtutor::write::tutorial_write_01::main();
+    csvtutor::write::tutorial_write_02::main();
 }
--- src/write.rs
+++ src/write.rs
@@ -1,1 +1,2 @@
 pub mod tutorial_write_01;
+pub mod tutorial_write_02;
